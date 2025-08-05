c23d5af040097ec4a6e1ee0e0007352f5a4cad78
Matthew C <matthew.chan654@education.nsw.gov.au>
2025-08-06 09:37:58 +1000

    2+3

--- 02.storingData/02.storingData.ino
+++ 02.storingData/02.storingData.ino
@@ -19,7 +19,22 @@
     https://www.arduino.cc/reference/en/language/variables/data-types/stringobject/
 */
 
+int myInt = 9600;
+int myIntValue = HIGH;
+String myString ("Hello World");
+float myFloatingPoint = 3.14;
+static unsigned long myUnsignedLong = 3.14;
+bool myvalue = false;
+
 void setup() {
+  Serial.begin(myInt);
+  Serial.print("Serial monitor is configured to ");
+  Serial.print(myInt);
+  Serial.print(" baud rate");
+  Serial.println("---");
+  // Print out the stored values
+  Serial.print("Data stored: ");
+  Serial.println(myString);
   
 }
 
